061fa1648322c456ae5975845b4a42c0e323008b
Alex Kharouk <akharoukpp@gmail.com>
2020-04-18 18:41:50 +0100

    Set up hls to work on non-safari browsers to play the m3u8 file

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,29 +1,40 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, createRef } from 'react'
 import Hls from 'hls.js'
 
-
+const videoSrc = 'https://stream.mux.com/E9mOC1Uh0202QvnCCyq501ZMUUuUAtdNj9m.m3u8'
 
 export default () => {
+  const videoRef = createRef(null)
+  
   useEffect(() => {
-    effect
-    return () => {
-      cleanup
+    const video = videoRef.current
+    if(Hls.isSupported()) {
+      const hls = new Hls()
+      hls.loadSource(videoSrc)
+      hls.attachMedia(video)
     }
-  }, [input])
+    
+    else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+      video.src = videoSrc
+    }
+
+
+  }, [videoRef])
 
   return (
     <div>
       <h2>Best Player</h2>
+      <video
+        ref={videoRef}
+        controls
+        src='https://stream.mux.com/E9mOC1Uh0202QvnCCyq501ZMUUuUAtdNj9m.m3u8'      
+      />
+      <br />
       {/* MP4 VIDEO: Not adaptive like HLS */}
       <video  
         controls
         muted
         src='https://storage.googleapis.com/muxdemofiles/mux-video-intro.mp4' 
-      />
-
-      <video 
-        controls
-        src='https://stream.mux.com/E9mOC1Uh0202QvnCCyq501ZMUUuUAtdNj9m.m3u8'      
       />
       </div>
     )
